f624a24e96ecbc4580a97e99973b883a0ea55596
krshna02 <abhinavsharma222002@gamil.com>
2024-07-27 16:00:46 +0530

    commit changes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,28 @@
+let score="22abc"
+let sc="22"
+let a=null
+let b=undefined
+let c=true
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber= Number(score);
+let valueInNumber1= Number(sc);
+let valueInNumber2= Number(a);
+let valueInNumber3= Number(b);
+let valueInNumber4= Number(c);
+console.log(typeof valueInNumber)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber) //"NaN"
+console.log(valueInNumber1) // "33"
+console.log(valueInNumber2) // null=0
+console.log(valueInNumber3); // undefined=undefined
+console.log(valueInNumber4); // boolean true=1,false =0 
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(somenumber)
+console.log(typeof stringnumber);
+let isloggedin=""
+let Bolleanisloggedin=Boolean(isloggedin);
+console.log(Bolleanisloggedin);
+
