e82d51936169565073942b348b1c9fb023f592f3
krshna02 <abhinavsharma222002@gamil.com>
2024-07-27 14:59:36 +0530

    commit changes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId=123455
+let accntMail="aa.gmail"
+var accntPsswrd="123455"
+accntcity="agra"
+accntMail="hn.gmail"
+accntPsswrd="232442"
+accntcity="delhi"
+let accntstate
+ console.log(accountId);
+ console.table([accntMail,accntPsswrd,accntcity,accntstate]);
+ /*dont usee var becz it does not allow scope*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+let age=18
+let name="krishna"
+let isloggedin= false
+//number >=2 to power 53
+//bigint
+//string >= ""
+//boolean >-true/false
+//null >= standalone value
+// undefined 
+// symbol >= umiqueness
+
+
+//object
+console.log(typeof undefined);
