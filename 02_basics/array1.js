99876a1b272aa207bd3a6e8bfe658784dee506c4
krshna02 <abhinavsharma222002@gamil.com>
2024-07-31 15:44:06 +0530

    yes

---
+++ 02_basics/array1.js
@@ -0,0 +1,23 @@
+// const arr=[1,2,3,4]
+// const heros=["shaktiman","ironman","america","ny",1]
+// console.log(heros[3]);
+const arr2= new Array( 2,1,4,3,5);
+// console.log(arr2);
+// console.log(arr[2]);
+// // array methods
+// arr2.push(5)// it add value int th end of array
+// console.log(arr2);
+// arr2.push(6)
+// arr2.pop()// it removes last value of array
+// console.log(arr2);
+// arr2.unshift(7)//its adds the value in the start of array
+// arr2.shift()// it removes 1st element of array
+// console.log(arr2);
+// const arr3=arr.join()//it changes datatype of array to string
+// console.log( typeof arr3);
+console.log("A",arr2)
+const myn1= arr2.slice(1,3)// it make changes on copy of array
+console.log("b",myn1);
+const myn2=arr2.splice(1,3)// it make changes on main array
+console.log("c",myn2);
+console.log(arr2)
