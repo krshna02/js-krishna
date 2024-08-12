6df3d98bcd6b37dc3afa06d937ed06bebd7b4be3
krshna02 <abhinavsharma222002@gamil.com>
2024-08-12 12:53:31 +0530

    func

--- 03_basics/03_scope.js
+++ 03_basics/03_scope.js
@@ -16,5 +16,39 @@ if(true){
 
  //console.log(b);// it will return error because b is not defined outside of if (the scope of b is inside {})
  // console.log(c);// it will return 8 becoz the scope of var is also outsied if statement
+function one(){
+    const username="krishna"
+     function two(){
+        const website="vscode"
+     console.log(username);
+     }
+     //console.log(website); it will give en arror becoz we accessing outside the scope of website
+     //closure -when child function can call parent funct element
+     two()
+}
+one()
+if(true){
+    const username="krishna"
+    if(username==="krishna"){
+        const website=" vscode"
+       console.log(username+website);
+        
+    }
+    //console.log(website);
+    
+}
+//console.log(username);
+
+//+++++++++++++++++++ MINI HOISTING+++++++++
+console.log(addone(5)) // ease hum pre intialize kr skte h 
+function addone(num1){
+    return num1+1
+}
+//addtwo(5) ease nhi kr skte agr ye kisi variable mai store h toh
+const addtwo=function(num1) { // if we assign in a variable then it will throw an error
+    return num1+2
+    
+}
+console.log(addtwo(5));
 
 
